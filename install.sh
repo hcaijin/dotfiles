2add2706abbe26bc76cfeaa8ba8d3bd8a51a5fae
hcaijin <hcjonline@gmail.com>
2020-04-13 01:14:42 +0800

    debug install

--- install.sh
+++ install.sh
@@ -11,118 +11,117 @@ Red='\033[0;31m'
 
 LOCKFILE=$PWD/dotfiles.lock
 
-check_package_manager(){
-    if type apt >/dev/null 2>&1; then
-      installpkg(){ apt-get install -y "$1" >/dev/null 2>&1; }
-    elif type pkg >/dev/null 2>&1; then
-      installpkg(){ pkg install -y "$1" >/dev/null 2>&1; }
-    elif type yum >/dev/null 2>&1; then
-      installpkg(){ yum install -y "$1" >/dev/null 2>&1; }
-    else
-      installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
-    fi
-}
+if type apt >/dev/null 2>&1; then
+    installpkg(){ apt-get install -y "$@" >/dev/null 2>&1; }
+elif type pkg >/dev/null 2>&1; then
+    installpkg(){ pkg install -y "$@" >/dev/null 2>&1; }
+elif type yum >/dev/null 2>&1; then
+    installpkg(){ yum install -y "$@" >/dev/null 2>&1; }
+else
+    installpkg(){ pacman --noconfirm --needed -S "$@" >/dev/null 2>&1 ;}
+fi
 
 # Symlinks the configs
 symlink () {
-  grep "$1" $LOCKFILE 2>/dev/null
-  if [ $? != 0 ]
-  then
-    echo $1 >> $LOCKFILE
-  fi
-  TARGET=$PWD/$2$1
-  FILE=$HOME/.$1
-  if [ -e "$FILE" ]
-  then
-    if file $FILE | grep $PWD &> /dev/null;then
-      printf "Installed $Red$FILE${Color_off}\n"
+	grep "$1" $LOCKFILE 2>/dev/null
+	if [ $? != 0 ]
+	then
+		echo $1 >> $LOCKFILE
+	fi
+	TARGET=$PWD/$2$1
+	FILE=$HOME/.$1
+	if [ -e "$FILE" ]
+	then
+		if file $FILE | grep $PWD &> /dev/null;then
+			printf "Installed $Red$FILE${Color_off}\n"
 
-  else
-      printf "Skipping $Red$FILE${Color_off}\n"
-    fi
-  else
-    printf "Linking $Cyan$FILE${Color_off} -> $Blue$TARGET${Color_off}\n"
-    ln -s "$TARGET" "$FILE"
-  fi
+		else
+			printf "Skipping $Red$FILE${Color_off}\n"
+		fi
+	else
+		printf "Linking $Cyan$FILE${Color_off} -> $Blue$TARGET${Color_off}\n"
+		ln -s "$TARGET" "$FILE"
+	fi
 }
 
 linkHiddenFile() {
-  for file in `ls hiddenrc`;
-  do
-    symlink $file hiddenrc/
-  done
+	for file in `ls hiddenrc`;
+	do
+		symlink $file hiddenrc/
+	done
 }
 
 linkFolder() {
-  for file in `ls $1 | egrep -v '^(etc|share)'`;
-  do
-    symlink $1/$file
-  done
+	for file in `ls $1 | egrep -v '^(etc|share)'`;
+	do
+		symlink $1/$file
+	done
 }
 
 main() {
-  # mkdir
-  if [ ! -d "$HOME/.config" ];then
-    mkdir ~/.config
-  fi
-  if [ ! -d "$HOME/.local/sbin" ];then
-    mkdir ~/.local/sbin
-  fi
-  # mail
-  if [ ! -f ~/.msmtprc ];
-  then
-    #cp msmtprc ~/.msmtprc
-    printf "copy ${red}msmtprc to ~/.msmtprc ${color_off}\n"
-    printf "you need to run chmod 0600 ~/.msmtprc after edit password\n"
+	# mkdir
+	if [ ! -d "$HOME/.config" ];then
+		mkdir ~/.config
+	fi
+	if [ ! -d "$HOME/.local/bin" ];then
+		mkdir -p ~/.local/bin
+	fi
+	# mail
+	if [ ! -f ~/.msmtprc ];
+	then
+		#cp msmtprc ~/.msmtprc
+		printf "copy ${red}msmtprc to ~/.msmtprc ${color_off}\n"
+		printf "you need to run chmod 0600 ~/.msmtprc after edit password\n"
 
-  fi
-  # Install folder to config or local
-  linkFolder config
-  linkFolder local/bin
-  linkFolder local/share
-  # Install hidden file to ~
-  linkHiddenFile
-  # Install FZF
-  if [ -e ~/.fzf ]
-  then
-    printf "Installed $Red~/.fzf$Color_off\n"
-  else
-    if [ -e ~/.fzf.orig ]
-    then
-      mv ~/.fzf.orig ~/.fzf
-      printf "$Blue Finished Installing fzf$Color_off\n"
-    else
-      printf "$Cyan Downloading  fzf -> $Blue$HOME/.fzf$Color_off\n"
-      git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-      ~/.fzf/install --all
-      printf "$Blue Finished Installing fzf$Color_off\n"
-    fi
-  fi
-  if [[ -e ~/.oh-my-zsh || ! `command -v zsh` ]]
-  then
-    printf "Unknow zsh command or Installed $Red~/.oh-my-zsh$Color_off\n"
-  else
-    if [ -e ~/.oh-my-zsh.orig ]
-    then
-      mv ~/.oh-my-zsh.orig ~/.oh-my-zsh
-      printf "$Blue Finished Installing oh-my-zsh$Color_off\n"
-    else
-      printf "$Cyan Downloading  oh-my-zsh -> $Blue$HOME/.oh-my-zsh$Color_off\n"
-      RUNZSH=no; CHSH=no; sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-      git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-      git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-      git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
-      [ -z "`grep "autoload -U compinit && compinit" ~/.zshrc`" ] && echo "autoload -U compinit && compinit" >> ~/.zshrc
-      sed -i '/^plugins=/c\plugins=(git z zsh-syntax-highlighting zsh-autosuggestions zsh-completions)' ~/.zshrc
-      sed -i '/^ZSH_THEME=/c\ZSH_THEME="ys"' ~/.zshrc
+	fi
+	# Install folder to config or local
+	linkFolder config
+	linkFolder local/bin
+	linkFolder local/share
+	# Install hidden file to ~
+	linkHiddenFile
+	# Install FZF
+	if [ -e ~/.fzf ]
+	then
+		printf "Installed $Red~/.fzf$Color_off\n"
+	else
+		if [ -e ~/.fzf.orig ]
+		then
+			mv ~/.fzf.orig ~/.fzf
+			printf "$Blue Finished Installing fzf$Color_off\n"
+		else
+			printf "$Cyan Downloading  fzf -> $Blue$HOME/.fzf$Color_off\n"
+			git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+			~/.fzf/install --all
+			printf "$Blue Finished Installing fzf$Color_off\n"
+		fi
+	fi
+	if [[ -e ~/.oh-my-zsh || ! `command -v zsh` ]]
+	then
+		printf "Unknow zsh command or Installed $Red~/.oh-my-zsh$Color_off\n"
+	else
+		if [ -e ~/.oh-my-zsh.orig ]
+		then
+			mv ~/.oh-my-zsh.orig ~/.oh-my-zsh
+			printf "$Blue Finished Installing oh-my-zsh$Color_off\n"
+		else
+			printf "$Cyan Downloading  oh-my-zsh -> $Blue$HOME/.oh-my-zsh$Color_off\n"
+			sh <(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended --keep-zshrc
+            ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+			git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+			git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+			git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
+			[ -z "`grep "autoload -U compinit && compinit" ~/.zshrc`" ] && echo "autoload -U compinit && compinit" >> ~/.zshrc
+			sed -i '/^plugins=/c\plugins=(git z zsh-syntax-highlighting zsh-autosuggestions zsh-completions)' ~/.zshrc
+			sed -i '/^ZSH_THEME=/c\ZSH_THEME="ys"' ~/.zshrc
 
-      printf "$Blue Finished Installing oh-my-zsh$Color_off\n"
-    fi
-  fi
-  # Pull github ssh pub key
-  sh <(curl -Ls https://raw.githubusercontent.com/hcaijin/SSH_Key_Installer/master/ikey.sh) -g hcaijin -p 2222 -d -i
+			printf "$Blue Finished Installing oh-my-zsh$Color_off\n"
+		fi
+	fi
 }
 
-check_package_manager
-installpkg "git wget curl vim zsh"
+installpkg git wget curl vim zsh
 main
+# Pull github ssh pub key
+sh <(curl -Ls https://raw.githubusercontent.com/hcaijin/SSH_Key_Installer/master/ikey.sh) -g hcaijin -d
+chsh -s `which zsh`
