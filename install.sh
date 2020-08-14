8797817cfadb89c80333b3c587a94f5f26960bca
hcaijin <hcjonline@gmail.com>
2020-08-15 00:47:37 +0800

    gtk size, i3block statusbar edit, sway edit, edit wofi config, zsh edit, add showclip autossh-do.sh etc.

--- install.sh
+++ install.sh
@@ -72,14 +72,6 @@ doUninstall() {
     fi
     mv ~/.fzf ~/.fzf.orig
   fi
-  if [ -e ~/.oh-my-zsh ]
-  then
-    if [ -e ~/.oh-my-zsh.orig ]
-    then
-      rm -rf ~/.oh-my-zsh.orig
-    fi
-    mv ~/.oh-my-zsh ~/.oh-my-zsh.orig
-  fi
   for file in `cat $LOCKFILE | sort -n | uniq`;
   do
     delink $file
@@ -166,28 +158,6 @@ doLink() {
 			git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 			~/.fzf/install --all
 			printf "$Blue Finished Installing fzf$Color_off\n"
-		fi
-	fi
-	if [[ -e ~/.oh-my-zsh || ! `command -v zsh` ]]
-	then
-		printf "Unknow zsh command or Installed $Red~/.oh-my-zsh$Color_off\n"
-	else
-		if [ -e ~/.oh-my-zsh.orig ]
-		then
-			mv ~/.oh-my-zsh.orig ~/.oh-my-zsh
-			printf "$Blue Finished Installing oh-my-zsh$Color_off\n"
-		else
-			printf "$Cyan Downloading  oh-my-zsh -> $Blue$HOME/.oh-my-zsh$Color_off\n"
-			sh <(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended --keep-zshrc
-            ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
-			# git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-			# git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-			# git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
-			[ -z "`grep "autoload -U compinit && compinit" ~/.zshrc`" ] && echo "autoload -U compinit && compinit" >> ~/.zshrc
-			# sed -i '/^plugins=/c\plugins=(git z zsh-syntax-highlighting zsh-autosuggestions zsh-completions)' ~/.zshrc
-			sed -i '/^ZSH_THEME=/c\ZSH_THEME="ys"' ~/.zshrc
-
-			printf "$Blue Finished Installing oh-my-zsh$Color_off\n"
 		fi
 	fi
 }
--- local/bin/tools/autossh-do.sh
+++ local/bin/tools/autossh-do.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 # Time: 2020-04-15 17:17:31
+# 本脚本只有反向代理功能,且服务端还要做nginx映射监听端口到所有主机
+#
+# 参考： https://blog.csdn.net/u012843189/article/details/79522738
+# 反向代理 ssh -fCNR 例子：ssh -fCNR [B机器IP或省略]:[B机器端口]:[A机器的IP]:[A机器的sshd端口] [登录B机器的用户名@B机器的IP] -p [B机器的sshd端口]
+# 正向代理 ssh -fCNL 例子：ssh -fCNL [*或省略]:[A机器端口]:[B机器的IP]:[B机器端口] [登录B机器的用户名@B机器的IP] -p [B机器的sshd端口]
+#-f 后台执行ssh指令
+#-C 允许压缩数据
+#-N 不执行远程指令
+#-R port:host:hostport 将远程主机(服务器)的某个端口转发到本地主机指定的端口,远程主机上分配了一个 socket 侦听 port 端口, 一旦这个端口上有了连接, 该连接就经过安全通道转向出去, 同时本地主机和 host 的 hostport 端口建立连接. 
+#-L port:host:hostport 将本地机(客户机)的某个端口转发到远端指定机器的指定端口，本地机器上分配了一个 socket 侦听 port 端口, 一旦这个端口上有了连接, 该连接就经过安全通道转发出去, 同时远程主机和 host 的 hostport 端口建立连接. 
+#-p 指定远程主机的端口
 
 USAGE () {
   echo "Usage:"
@@ -16,7 +27,7 @@ if [ $# -lt 4 ]; then
   exit -1
 fi
 
-while getopts "p:l:h:" OPT; do
+while getopts "ap:l:h:" OPT; do
   case $OPT in
     p)
       REMOTE_PORT=$OPTARG
