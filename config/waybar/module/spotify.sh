b6c16435543de98c3badb2fcd8a581f598afc460
hcaijin <hcjonline@gmail.com>
2020-08-15 18:58:32 +0800

    del spotify.sh

--- config/waybar/module/spotify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
-if [[ ${#info} > 40 ]]; then
-    info=$(echo $info | cut -c1-40)"..."
-fi
-
-echo "$info"
