d733d71e148eac9eee7e6bbfbb5dc3c9741e069d
hcaijin <hcjonline@gmail.com>
2020-10-11 17:11:13 +0800

    waybar debug

--- config/waybar/module/neofetch.sh
+++ config/waybar/module/neofetch.sh
@@ -2,6 +2,34 @@
 
 FETCHLOG="${XDG_CACHE_HOME:-$HOME/.cache}/neofetch.log"
 
-[ -f "$FETCHLOG" ] || neofetch > $FETCHLOG
+printlogo() {
+  [ -f "$FETCHLOG" ] || neofetch > $FETCHLOG
+  printf "%s" $(cat $FETCHLOG | grep 'OS' | awk '{print $2$3}')
+}
 
-printf "%s" $(cat $FETCHLOG | grep 'OS' | awk '{print $2$3}')
+refresh() {
+  neofetch > $FETCHLOG
+  notify-check-send "Neofetch" "updating..."
+  sleep 1
+  showfetch
+}
+
+showfetch(){
+  $TERMINAL --display=$DISPLAY --hold -t "neofetch" -e "cat $FETCHLOG"
+}
+
+case "$1" in
+  logo)
+    printlogo
+    ;;
+  show)
+    showfetch
+    ;;
+  refresh)
+    refresh
+    ;;
+  *)
+    echo "Usag: $0 {logo|show|refresh}"
+    exit
+    ;;
+esac
